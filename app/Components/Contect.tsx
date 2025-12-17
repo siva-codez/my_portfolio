1e8c2b1c10b7efd21ee33609eeca7c397f96529a
siva-tuty <sivaarumugaaperumal@gmail.com>
2025-12-17 21:54:15 +0530

    fourth commit

--- app/Components/Contect.tsx
+++ app/Components/Contect.tsx
@@ -9,7 +9,7 @@ const Contect = () => {
       <section id="contact" className="bg-[#0f0f0f] text-white pt-1  py-20 px-4 sm:px-6 lg:px-8">
         <div className="max-w-4xl mx-auto text-center">
           <h2 className="text-4xl font-bold mb-4">
-            Contect <span className="text-pink-500">Me</span>
+            Contact <span className="text-pink-500">Me</span>
           </h2>
           <p className="text-xl text-gray-300 mb-12">
             I'm always open to discussing new projects, creative ideas, or opportunities to be part of your vision.
